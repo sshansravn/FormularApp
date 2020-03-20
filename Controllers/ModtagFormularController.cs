168846ddec928194bc4ffb89a2ccfd4d5be6cda0
Hans Ravnsfjall <hra@sendistovan.fo>
2020-03-20 14:10:54 +0000

    Initial commit

---
+++ Controllers/ModtagFormularController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using FormularApp.Models;
+
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace FormularApp.Controllers
+{
+
+
+
+    public class ModtagFormularController : Controller
+    {
+        // GET: /<controller>/
+        /* public IActionResult Index()
+         {
+             return View();
+         }
+         */
+
+        [HttpPost]
+        public IActionResult Index(string navn, string email, string telefon)
+        {
+            var fieldNavn = navn;
+            var fieldEmail = email;
+            var fieldTelefon = telefon;
+
+            var formularModel = new FormModel(){ navn = fieldNavn, email=fieldEmail, telefon=fieldTelefon };
+            return View(formularModel);
+        }
+        
+        /*[HttpPost]
+        public IActionResult Index(string navn, string email, string telefon)
+        {
+            var formnavn = navn;
+            var formemail = email;
+            var formtelefon = telefon;
+
+            var FormModel = new FormModel()
+            {navn = "Ha";
+            
+        
+            }
+           
+        }*/
+    }
+}
---
+++ Models/FormModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using System.Linq;
+
+namespace FormularApp.Models
+{
+    public class FormModel
+    {
+        public string navn { get; set; }
+        public string email { get; set; }
+        public string telefon { get; set; }
+
+    }
+}
